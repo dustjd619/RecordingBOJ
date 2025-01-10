923f39c847222d93a0b6aefda0c9b4dbe38afe76
Yeonseong Shin <dustjd2651@gmail.com>
2025-01-10 17:43:59 +0900

    [Bronze II] Title: 다이얼, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -0,0 +1,22 @@
+N = list(input())
+
+time = 0
+for i in N:
+    if i in ["A", "B", "C"]:
+        time += 3
+    elif i in ["D", "E", "F"]:
+        time += 4
+    elif i in ["G", "H", "I"]:
+        time += 5
+    elif i in ["J", "K", "L"]:
+        time += 6
+    elif i in ["M", "N", "O"]:
+        time += 7
+    elif i in ["P", "Q", "R", "S"]:
+        time += 8
+    elif i in ["T", "U", "V"]:
+        time += 9
+    else:
+        time += 10
+
+print(time)
