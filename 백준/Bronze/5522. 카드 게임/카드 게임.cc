168c6d1db96c3fb4b61289bd2af0106dc80a9e49
Yeonseong Shin <dustjd2651@gmail.com>
2025-01-16 01:47:10 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int score, sum = 0;
+
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> score;
+        sum = sum + score;
+    }
+
+    cout << sum;
+}
