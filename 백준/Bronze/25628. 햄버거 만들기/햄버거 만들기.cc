25b527a8ba4a8611eeb30ba3ce1cb1ccfa0a5bf7
Yeonseong Shin <dustjd2651@gmail.com>
2025-01-23 18:45:07 +0900

    [Bronze IV] Title: 햄버거 만들기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/25628. 햄버거 만들기/햄버거 만들기.cc
+++ 백준/Bronze/25628. 햄버거 만들기/햄버거 만들기.cc
@@ -5,19 +5,5 @@ int main()
 {
     int A, B;
     cin >> A >> B;
-
-    int cnt = 0;
-    while (true)
-    {
-        if (A <= 1 || B == 0)
-        {
-            break;
-        }
-
-        A -= 2;
-        B -= 1;
-        cnt++;
-    }
-
-    cout << cnt;
+    cout << min(A / 2, B);
 }
