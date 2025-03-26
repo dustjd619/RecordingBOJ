c1f918fe41c6b59ae1ba0c9515f3c8afc76e0c47
Yeonseong Shin <dustjd2651@gmail.com>
2025-03-26 16:44:43 +0900

    [Gold V] Title: 밥, Time: 240 ms, Memory: 119268 KB -BaekjoonHub

---
+++ 백준/Gold/23559. 밥/밥.py
@@ -0,0 +1,20 @@
+N, X = map(int, input().split())
+F = list()
+
+sum = 0
+for i in range(N):
+    a, b = map(int, input().split())
+    F.append((a, b))
+    sum += b
+    X -= 1000
+
+F.sort(key=lambda x: x[0] - x[1], reverse=True)
+
+for i in range(N):
+    if X >= 4000 and F[i][0] - F[i][1] > 0:
+        sum = sum - F[i][1] + F[i][0]
+        X -= 4000
+    elif X < 0:
+        break
+
+print(sum)
