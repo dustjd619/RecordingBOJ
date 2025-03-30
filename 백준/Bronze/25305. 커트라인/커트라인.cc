4e82c86da5f481ae4fd93b0757ab8f940a116d79
Yeonseong Shin <dustjd2651@gmail.com>
2025-03-31 00:48:06 +0900

    [Bronze II] Title: 커트라인, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int N, k;
+    cin >> N >> k;
+
+    vector<int> score(N);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> score[i];
+    }
+
+    sort(score.begin(), score.end(), greater<int>());
+
+    cout << score[k - 1];
+}
