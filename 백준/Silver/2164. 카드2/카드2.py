51a4d34e12a0ca0470fca37a722c50dd3b50fd7e
Yeonseong Shin <dustjd2651@gmail.com>
2025-05-06 20:50:02 +0900

    [Silver IV] Title: 카드2, Time: 128 ms, Memory: 117340 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -2,16 +2,11 @@ from collections import deque
 
 N = int(input())
 
-dq = deque()
-for i in range(1, N + 1):
-    dq.append(i)
+a = deque(range(1, N + 1))
 
-while len(dq) != 1:
-    dq.popleft()
 
-    if len(dq) != 0:
-        a = dq[0]
-        dq.popleft()
-        dq.append(a)
+while len(a) != 1:
+    a.popleft()
+    a.rotate(-1)
 
-print(dq[0])
+print(str(a[0]))
